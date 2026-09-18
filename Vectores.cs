b739f860da9b74bfeed694094841830d29f12fe8
diegoxvargas <dandresvargas@unicaribe.edu.co>
2026-09-18 13:02:29 -0500

    Add Vectores.cs with basic array example

---
+++ Vectores.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[5]
+            { 7, 6, 5, 2, 8 };
+            Console.WriteLine($"Elemento en [0]: {num[0]}");
+
+
+        }
+    }
+}
